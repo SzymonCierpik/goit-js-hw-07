3325a03347c57cda958eca009ba9e3ee6f047198
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-02-27 12:08:34 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,34 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+function buildGallery(items) {
+  const galleryElement = document.querySelector(".gallery");
+  const galleryMarkup = items
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </div>
+    `;
+    })
 
-galleryItems.forEach((item) => {
-  const galleryItem = `<div class="gallery__item">
-  
-  <a class="gallery__link" href="${item.original}">
+    .join("");
 
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="large-image.jpg"
-      alt="${item.description}"
-    />
-  </a>
-</div>`;
+  galleryElement.innerHTML = galleryMarkup;
+}
 
-  gallery.insertAdjacentElement("beforeend", galleryItem);
-});
+buildGallery(galleryItems);
 
-console.log(galleryItems);
+const clickGalleryItname 
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+   <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+    </div>
+`);
+
+instance.show();
