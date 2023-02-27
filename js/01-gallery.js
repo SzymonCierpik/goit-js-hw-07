65815f152423fe2614148c1ef26d769a82cdfcde
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-02-27 10:49:29 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const galleryItem = `<div class="gallery__item">
+  
+  <a class="gallery__link" href="${item.original}">
+
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="large-image.jpg"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+
+  gallery.insertAdjacentElement("beforeend", galleryItem);
+});
 
 console.log(galleryItems);
